076da44d00b790292898f29c65bb77dfef9339dd
filippogambarota <filippo.gambarota@gmail.com>
2024-12-20 13:21:31 -0500

    adding auto git commit for _book and _freeze

--- quarto-scripts/post-render.R
+++ quarto-scripts/post-render.R
@@ -1,1 +1,14 @@
+# render the rmd
 rmarkdown::render("README.rmd", clean = TRUE, quiet = TRUE)
+
+# git add all _book, detailed commit on qmd files
+
+system("git add _book/")
+system("git commit -m 'updating book'")
+
+# git add all _freeze/
+system("git add _freeze/")
+system("git commit -m 'updating quarto cache'")
+
+system("git add todoget.md")
+system("git commit -m 'updating todo list'")
